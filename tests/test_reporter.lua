3d0091b522d08918d987b782539b9bb53954d3dc
Matt Layman <matthewlayman@gmail.com>
2023-07-04 14:26:06 -0400

    Finish off reporter tests.
    For #7

--- tests/test_reporter.lua
+++ tests/test_reporter.lua
@@ -118,8 +118,91 @@ function tests.test_start_test()
     assert.is_equal("tests/test_something.lua::test_foo ", get_content(file))
 end
 
--- TODO: finish_test
--- TODO: show failure details
--- TODO: summarize
+-- Finish test with passing result.
+function tests.test_finish_test_passing()
+    local config = {}
+    local file = io.tmpfile()
+    local reporter = Reporter(config, file)
+
+    reporter:finish_test("tests/test_something.lua", "test_foo", true, nil)
+
+    assert.is_equal("\x1B[0m\x1B[32m.\x1B[0m", get_content(file))
+    assert.is_equal(1, reporter._passed_count)
+end
+
+-- Finish test with passing result verbose.
+function tests.test_finish_test_passing_verbose()
+    local config = {verbose = true}
+    local file = io.tmpfile()
+    local reporter = Reporter(config, file)
+
+    reporter:finish_test("tests/test_something.lua", "test_foo", true, nil)
+
+    assert.is_equal("\x1B[0m\x1B[32mPASSED\n\x1B[0m", get_content(file))
+end
+
+-- Finish test with failing result.
+function tests.test_finish_test_failing()
+    local config = {}
+    local file = io.tmpfile()
+    local reporter = Reporter(config, file)
+
+    reporter:finish_test("tests/test_something.lua", "test_foo", false,
+                         "details")
+
+    assert.is_equal("\x1B[0m\x1B[31mF\x1B[0m", get_content(file))
+    assert.is_equal(0, reporter._passed_count)
+    assert.is_same({["tests/test_something.lua"] = {test_foo = "details"}},
+                   reporter._failures)
+end
+
+-- Finish test with failing result verbose.
+function tests.test_finish_test_failing_verbose()
+    local config = {verbose = true}
+    local file = io.tmpfile()
+    local reporter = Reporter(config, file)
+
+    reporter:finish_test("tests/test_something.lua", "test_foo", false,
+                         "details")
+
+    assert.is_equal("\x1B[0m\x1B[31mFAILED\n\x1B[0m", get_content(file))
+end
+
+-- Summarize reports with no failures.
+function tests.test_summarize_no_failures()
+    local config = {}
+    local file = io.tmpfile()
+    -- local file = io.open('test-out.txt', 'w')
+    local reporter = Reporter(config, file)
+    reporter:start()
+    reporter:finish()
+
+    local final_status = reporter:summarize()
+
+    assert.is_equal(
+        "\x1B[0m\x1B[1m\x1B[32m0 passed \x1B[0m\x1B[32min 0.0s\x1B[0m\n",
+        get_content(file))
+    assert.is_equal(0, final_status)
+end
+
+-- Summarize reports with failure.
+function tests.test_summarize_failure()
+    local config = {}
+    local file = io.tmpfile()
+    -- local file = io.open('test-out.txt', 'w')
+    local reporter = Reporter(config, file)
+    reporter._failures = {["tests/test_something.lua"] = {test_foo = "details"}}
+    reporter:start()
+    reporter:finish()
+
+    local final_status = reporter:summarize()
+
+    local failure_details =
+        "\x1B[0m\x1B[1m\x1B[31mtests/test_something.lua::test_foo\x1B[0m\n\ndetails\n\x1B[0m\n"
+    local summary =
+        "\x1B[0m\x1B[1m\x1B[31m1 failed\x1B[0m, \x1B[32m0 passed \x1B[31min 0.0s\x1B[0m\n"
+    assert.is_equal(failure_details .. summary, get_content(file))
+    assert.is_equal(1, final_status)
+end
 
 return tests
