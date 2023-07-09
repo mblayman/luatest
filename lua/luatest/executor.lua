6b516974f4334eb9ccbd7d27d4e65d38091b2dfd
Matt Layman <matthewlayman@gmail.com>
2023-07-09 18:32:30 -0400

    Make execution use the new collection structure.

--- lua/luatest/executor.lua
+++ lua/luatest/executor.lua
@@ -5,8 +5,9 @@ local function execute(test_modules, reporter)
     for relpath, module_info in tablex.sort(test_modules) do
         if relpath ~= "meta" then
             reporter:start_module(relpath)
-            for test_name, test in tablex.sort(module_info.module) do
+            for _, test_name in tablex.sort(module_info.tests) do
                 reporter:start_test(relpath, test_name)
+                local test = module_info.module[test_name]
                 local status, assertion_details = pcall(test)
                 reporter:finish_test(relpath, test_name, status,
                                      assertion_details)
