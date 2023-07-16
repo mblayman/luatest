c95d382526930b211141e68bd241d7b9e4d2175f
Matt Layman <matthewlayman@gmail.com>
2023-07-16 16:40:29 -0400

    Validate the coverage directory. (#28)
    Fixes #26

--- lua/luatest/coverage.lua
+++ lua/luatest/coverage.lua
@@ -4,12 +4,25 @@ local runner = require "luacov.runner"
 local dir = require "pl.dir"
 local path = require "pl.path"
 
+local Reporter = require "luatest.reporter"
+
+-- Validate that the coverage directory exists.
+local function validate_coverage_directory(coverage_directory)
+    if not path.isdir(coverage_directory) then
+        Reporter.fatal_early(coverage_directory ..
+                                 " is not a valid directory for coverage.")
+    end
+end
+
 -- Set all the files that should be measured for coverage.
 local function set_included_files(config, configuration)
     -- Include all source files in luacov format
     -- (i.e. with forward slash and no extension)
     local cwd = path.currentdir()
     local src = path.join(cwd, config.cov)
+
+    validate_coverage_directory(src)
+
     for root, _, files in dir.walk(src) do
         for file_ in files:iter() do
             local filepath = path.join(root, file_)
@@ -50,5 +63,6 @@ local function finalize_coverage() runner.shutdown() end
 return {
     initialize_coverage = initialize_coverage,
     finalize_coverage = finalize_coverage,
-    set_included_files = set_included_files
+    set_included_files = set_included_files,
+    validate_coverage_directory = validate_coverage_directory
 }
